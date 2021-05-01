ed6f837a53d5a28ce1ce186c34bb14c1e04e6e9a
Shantanu Shubham <shantanu0707@gmail.com>
2021-05-01 05:46:58 +0530

    [Add] QR Scanner to Send Money

--- phonepe-react/src/components/SendPCashScreen.js
+++ phonepe-react/src/components/SendPCashScreen.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import QRSCanner from "react-qr-scanner";
+import "../css/General.css";
 
 const SendPCashScreen = () => {
-  return <h1>This is Send P Cash Screen</h1>;
+  const [scannedResult, setScannedResult] = useState(null);
+  //
+  return (
+    <>
+      <div className={"scan-camera"}>
+        <h2 className={"scan-qr-mssg"}>Scan Receiver's QR</h2>
+        <QRSCanner
+          delay={false}
+          onScan={
+            scannedResult === null
+              ? (data) => {
+                  console.log(data);
+                  setScannedResult(data);
+                }
+              : null
+          }
+          onError={(error) => console.error(error)}
+        />
+      </div>
+      <div className={"scan-button-box"}>
+        <button
+          className={"scan-button"}
+          onClick={(e) => setScannedResult(null)}
+        >
+          Scan Again
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default SendPCashScreen;
