5e526effe6aca671a1a5d5660c873a4c3772f4e5
prakharshriv <prakharshriv@gmail.com>
2021-05-01 16:48:19 +0530

    queue manager

--- phonepe-backend/services/bank.js
+++ phonepe-backend/services/bank.js
@@ -4,6 +4,7 @@ var transactionModel=require('../models/transaction')
 var phonePeBankModel=require('../models/phonepeBank')
 var userModel=require('../models/user')
 var shortid=require('shortid')
+var ledgerModel=require('../models/ledger')
 class bank{
     constructor(){
 
@@ -60,6 +61,54 @@ class bank{
             return({success:true,user})
         } catch (error) {
             return({success:false})
+        }
+    }
+    async addTransactionsToLedger(transactions)
+    {
+        try {
+            let txnList=await ledgerModel.insertMany(transactions)
+            return({success:true})
+        } catch (error) {
+            return({success:false})
+        }
+    }
+    async resolveLedger()
+    {
+        try {
+            let ledgerResolve= await ledgerModel.aggregate([
+                {$match:{resolved:false}},
+                {$group:{
+                    _id:{transactionId:"$transactionId"},
+                    transactions:{$push:{transactionId:'$transactionId',receiver:'$receiver',amount:'$amount'}}
+                }}
+            ])  
+            let resolved=[]
+            ledgerResolve.forEach(el=>{
+                if(el.transactions.length==2)
+                {
+                    resolved.push({transactionId:el.transactions[0].transactionId,
+                        receiver:el.transactions[0].receiver,
+                        amount:el.transactions[0].amount
+                    })
+
+                }
+            })
+            this.creditMoney(ledgerResolve)
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async creditMoney(receiverList){
+        try {
+            receiverList.forEach(async (el)=>{
+                await ledgerModel.updateMany({transactionId:el.transactionId},{resolved:true})
+                let getData=await bankModel.findOne({upiId:el.receiver})
+                let updateData=await bankModel.findOneAndUpdate({upiId:el.receiver},{amount:getData.amount+el.amount})
+            })
+            console.log("updated",receiverList);
+        } catch (error) {
+            console.log(error);
         }
     }
 }
